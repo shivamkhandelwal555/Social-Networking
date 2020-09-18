77eee949e0a34a5dfb06d6e13168b23e86e146bc
shivam <shivamkhandelwal555@gmail.com>
2020-09-19 00:14:43 +0530

    Added the first migrations

--- sn_users/views.py
+++ sn_users/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "comments": ["quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"],
+    "likes": 28,
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "comments": ["est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"],
+    "likes": 12,
+  },
+]
 
 def index(request):
-    return render(request, 'index.html')
+    results = {
+        'posts': posts
+    }
+    return render(request, 'index.html', results)
